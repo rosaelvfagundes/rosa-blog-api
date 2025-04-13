2e3aed788d6e6993eb7fc965903854b8aa124daf
Rosael Fagundes <rosael.fagundes@gmail.com>
2025-04-13 19:15:14 -0300

    feat: iniciando o projeto

---
+++ src/main/java/com/decolatech/rosa_blog_api/RosaBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.decolatech.rosa_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosaBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosaBlogApiApplication.class, args);
+	}
+
+}
