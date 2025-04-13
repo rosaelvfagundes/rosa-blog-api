7372f224543c40b29024620688163ca41690c6d1
Rosael Fagundes <rosael.fagundes@gmail.com>
2025-04-13 19:40:02 -0300

    feat: criando entity e repository

---
+++ src/main/java/com/decolatech/rosa_blog_api/repository/BlogPostRepository.java
@@ -0,0 +1,8 @@
+package com.decolatech.rosa_blog_api.repository;
+
+import com.decolatech.rosa_blog_api.entity.BlogPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long> { }
