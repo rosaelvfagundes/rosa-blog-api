20a333998b67090f13bd4e85331c76d0f3bbfeee
Rosael Fagundes <rosael.fagundes@gmail.com>
2025-04-13 21:45:22 -0300

    refactor: reiniciando desenvolvimento

--- src/main/java/com/decolatech/rosa_blog_api/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.decolatech.rosa_blog_api.repository;
-
-import com.decolatech.rosa_blog_api.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
