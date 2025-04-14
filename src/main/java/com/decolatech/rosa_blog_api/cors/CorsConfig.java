91b1df09ace896a792719ffd4be4d6d0407d7345
Rosael Fagundes <rosael.fagundes@gmail.com>
2025-04-14 02:47:02 -0300

    feat: configuração de cors

---
+++ src/main/java/com/decolatech/rosa_blog_api/cors/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.decolatech.rosa_blog_api.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // Permitir qualquer origem
+        config.addAllowedOriginPattern("*");
+        // Permitir quaisquer cabeçalhos
+        config.addAllowedHeader("*");
+        // Permitir quaisquer métodos HTTP
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
